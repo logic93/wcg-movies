da45aa7b8b2e53331fc7145c14b7317557b9630e
logic93 <jeanmelki1@gmail.com>
2024-06-26 19:20:51 +0200

    Add api requests

---
+++ src/api/index.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import { BASE_URL } from "@/utils/constants";
+
+export async function fetchMovieByTitle(query: string) {
+  try {
+    const res = await fetch(`${BASE_URL}&t=${query}`);
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function fetchMovie(query: string) {
+  try {
+    let data;
+    const res = await fetch(`${BASE_URL}&s=${query}`);
+    data = await res.json();
+
+    if (data.Response === "False") {
+      const res = await fetch(`${BASE_URL}&i=${query}`);
+      data = await res.json();
+    }
+
+    if (data.Response === "True") {
+      if (data.Search && Array.isArray(data.Search)) {
+        return data.Search;
+      }
+
+      if (data && typeof data === "object") {
+        return [data];
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
